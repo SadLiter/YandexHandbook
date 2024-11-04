2f8f614f6d7b83050f504ac44c3d6f4aed20e9ba
SadLiter <valentinvolkovgg@gmail.com>
2024-11-04 16:35:44 +0300

    new file

---
+++ C++/Печать_текста.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n\
+  - is a better C\n\
+  - supports data abstraction\n\
+  - supports object-oriented programming\n\
+  - supports generic programming.";
+}
